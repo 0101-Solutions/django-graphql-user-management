8cb5a383e3ca9616f611a3d3db7444d4909897ff
Toxic254 <cyrus_g@outlook.com>
2022-01-21 14:50:50 +0300

    Created a custom ExtendedUser model to be used as our base model.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+
+class ExtendedUser(AbstractUser):
+
+    email = models.EmailField(blank=False, verbose_name="email")
+
+    USERNAME_FIELD = "username"
+    EMAIL_FIELD = "email"
+
